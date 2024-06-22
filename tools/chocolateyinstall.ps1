73bd9ecea7007d97bdf6a7031bde8ef68a05fc3e
kjxbyz <kjxbyz888@outlook.com>
2024-06-22 23:13:48 +0800

    chore: v0.0.1-rc23

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/cyf/faforever/releases/download/v0.0.0%2B21/FaForever_0.0.0.21_x86_en-US.msi'
-$url64      = 'https://github.com/cyf/faforever/releases/download/v0.0.0%2B21/FaForever_0.0.0.21_x64_en-US.msi'
+$url        = 'https://github.com/cyf/faforever/releases/download/v0.0.1%2B23/FaForever_0.0.1.23_x86_en-US.msi'
+$url64      = 'https://github.com/cyf/faforever/releases/download/v0.0.1%2B23/FaForever_0.0.1.23_x64_en-US.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'faforever*'
 
-  checksum      = ''
+  checksum      = 'DDFBC4371F9CAD99CBF3AF6DE06078A533E2C4B92BC249A5091BCB821B9FEF33'
   checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = '7D452D0552A4A891AB1BFBF69953E725E4FCF032665EA897C139BFE60F35F4BF'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
